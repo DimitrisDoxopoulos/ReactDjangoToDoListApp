50e12e3e7ce3cbe9c68fd6bc88a15d28fbb83594
DimitrisDoxopoulos <olonir@hotmail.com>
2024-03-09 15:12:02 +0200

    remade the whole backend api

--- NotesApi/notes_api/admin.py
+++ NotesApi/notes_api/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from notes_api import models
+from django.utils.translation import gettext_lazy as _
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ["email"]
+    add_form = [CustomUserCreationForm]
+    form = CustomUserChangeForm
+    model = models.User
+    list_display = ["email", "first_name", "last_name", "is_staff", "is_active"]
+    list_display_links = ["email"]
+    list_filter = ["email", "first_name", "last_name", "is_staff", "is_active"]
+    search_fields = ["email", "first_name", "last_name"]
+
 
 # Register your models here.
 admin.site.register(models.Todo)
+admin.site.register(models.User, UserAdmin)
--- NotesApi/notes_api/models.py
+++ NotesApi/notes_api/models.py
@@ -1,6 +1,9 @@
 from django.utils import timezone
-
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManger
+
 
 # Create your models here.
 class Todo(models.Model):
@@ -12,3 +15,28 @@ class Todo(models.Model):
 
     def __str__(self):
         return self.body
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(_("First Name"), max_length=100)
+    last_name = models.CharField(_("Last Name"), max_length=100)
+    email = models.EmailField(_("Email Address"), max_length=254, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = CustomUserManger()
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
